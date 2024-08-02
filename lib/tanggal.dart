4982f49a19b657f467c7bc947c393aa630f5e138
zuhri41code <faizidn4@gmail.com>
2024-08-02 09:22:38 +0700

    first commit

---
+++ lib/tanggal.dart
@@ -0,0 +1,53 @@
+void main () {
+
+ String kodesiswa =  "hf" ;
+
+switch (kodesiswa) {
+  case 'as':
+print('asep');
+break;
+case 'br':
+print('bryan');
+break;
+case 'hf':
+print('hafiz');
+break;
+case 'bd':
+print('budi');
+break;
+case 'kr':
+print('kira');
+break;
+case 'ns':
+print('nisa');
+break;
+case 'kk':
+print('kiki');
+break;
+case 'ro':
+print('riko');
+break;
+case 'rt':
+print('rahmat');
+break;
+case 'jm':
+print('james');
+break;
+case 'rn':
+print('rehan');
+break;
+case 'an':
+print('aron');
+break;
+case 'wn':
+print('wisnu');
+break;
+case 'rk':
+print('riska');
+break;
+}
+
+
+
+
+}
